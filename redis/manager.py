b7ff175d101131562007389ab0ccce50b613784a
danielconchaa-ux <daniel.concha_a@mail.udp.cl>
2026-05-30 22:24:53 -0400

    Update manager.py
    Ahora recibe data a traves de kafka del send y respuestas, y envia a traves de kafka a respuestas

--- redis/manager.py
+++ redis/manager.py
@@ -2,73 +2,105 @@ from flask import request, Flask, jsonify
 import redis
 import requests
 import time
+from confluent_kafka import Producer, Consumer
+import json
 
 r = redis.Redis(host='redis-db', port=6379, decode_responses=True)
 app = Flask(__name__)
 
-@app.route('/data', methods=['POST'])
-def receive_data():
-    data = request.get_json() #automatically parses the JSON you sent
-   # data = request.get_json
-    print("At recieve")
-    Q=data.get('Q')
-    Z=data.get('Z')
-    Sums=Q*10+Z
-    times=time.time()
-    times=times+data.get('time')
+# 1. Configuration uses a dictionary of Kafka properties
+conf = {
+    'bootstrap.servers': 'kafka:29092',
+    'group.id': 'redis',
+    'auto.offset.reset': 'earliest' # Start from the beginning if no offset exists
+}
 
-    Q1=data.get('Q1')
-    Z1=data.get('Z1')
+prod = Producer({'bootstrap.servers': 'kafka:29092'})
 
-    check=r.exists(str(Sums))
-    print("Check: ", check)
-    if check == 0:   #send to respuesta
-        print("send to backend")
-        response = requests.post("http://node-app:5000/data", json={"Q":Q,"Z":Z,"time":times})
-        print(response.json())
-        r.set(str(Sums),response.text, ex=100)
-        print("at 0")
-        print(r.get(str(Sums)))
-        return jsonify(response.json()), 200
-    else:
-        cache_response = r.get(str(Sums))
-        print("at 1")
-        requests.post("http://csv-writer:5000/data", json={"Q":Q,"Z":Z,"R":cache_response, "Tasa" : "Hit","time":times})
-        return jsonify({"data" : cache_response}), 200
-    
-@app.route('/data2', methods=['POST'])
-def receive_data_2():
-    data = request.get_json() #automatically parses the JSON you sent
-   # data = request.get_json
-    print("At recieve")
-    Q=data.get('Q')
-    Z=data.get('Z')
-    Z2=data.get('Z2')
-    Sums=Q*100+Z*10+Z2
-   # times=time.time()
-    times=data.get('time')
+# Initialize the consumer
+consumer = Consumer(conf)
 
-    Q1=data.get('Q1')
-    Z1=data.get('Z1')
+# 2. Subscribe to the topic(s)
+consumer.subscribe(['queries','queries2','respuestas','respuestas2'])
 
-    check=r.exists(str(Sums))
-    print("Check: ", check)
-    if check == 0:   #send to respuesta
-        print("send to backend")
-        response = requests.post("http://node-app:5000/data2", json={"Q":Q,"Z":Z,"Z2":Z2,"time":times})
-        print(response.json())
-        r.set(str(Sums),response.text)
-        print("at 0")
-        print(r.get(str(Sums)))
-        return jsonify(response.json()), 200
-    else:
-        cache_response = r.get(str(Sums))
-        Zones=Z*10+Z2
-        print("at 1")
-        requests.post("http://csv-writer:5000/data", json={"Q":Q,"Z":Zones,"R":cache_response, "Tasa" : "Hit","time":times})
-        return jsonify({"data" : cache_response}), 200
+
+
 
 if __name__ == "__main__":
     # In Docker, you MUST use host='0.0.0.0' to be reachable
     print("manager runin")
-    app.run(host='0.0.0.0', port=5000)
+    
+    while True :
+
+        msg = consumer.poll(1.0)
+        if msg is None: continue
+        if msg.error(): continue
+
+        data = json.loads(msg.value().decode('utf-8'))
+        topic_name = msg.topic()
+
+        if topic_name == 'queries':
+    # data = request.get_json
+            print("At recieve")
+            Q=data["Q"]
+            Z=data["Z"]
+            Sums=Q*10+Z
+            times=data["T"]
+
+            check=r.exists(str(Sums))
+            print("Check: ", check)
+            if check == 0:   #send to respuesta
+                print("send to backend")
+                prod.produce('backend',value=json.dumps(data).encode('utf-8'))
+                
+            else:
+                cache_response = r.get(str(Sums))
+                print("at 1")
+                requests.post("http://csv-writer:5000/data", json={"Q":Q,"Z":Z,"R":cache_response, "Tasa" : "Hit","time":times})
+        
+        elif topic_name=='queries2':
+            print("At recieve")
+            Q=data["Q"]
+            Z=["Z"]
+            Z2=["Z2"]
+            Sums=Q*100+Z*10+Z2
+            times=data["T"]
+
+            Q1=data.get('Q1')
+            Z1=data.get('Z1')
+
+            check=r.exists(str(Sums))
+            print("Check: ", check)
+            if check == 0:   #send to respuesta
+                print("send to backend")
+                prod.produce('backend2',value=json.dumps(data).encode('utf-8'))
+            else:
+                cache_response = r.get(str(Sums))
+                Zones=Z*10+Z2
+                print("at 1")
+                requests.post("http://csv-writer:5000/data", json={"Q":Q,"Z":Zones,"R":cache_response, "Tasa" : "Hit","time":times})
+
+        elif topic_name == 'respuestas':
+            print("Respuesta1")
+            Q=data["Qr"]
+            Z=data["Zr"]
+            Sums=Q*10+Z
+            times=data["T"]
+            Res = data["R"]
+
+            r.set(str(Sums),Res, ex=100)
+            print("at 0")
+            print(r.get(str(Sums)))
+        
+        elif topic_name == 'respuestas2':
+            print("Respuesta2")
+            Q=data["Qr"]
+            Z=data["Zr"]
+            Z2=["Z2r"]
+            Sums=Q*100+Z*10+Z2
+            times=data["T"]
+            Res = data["R"]
+
+            r.set(str(Sums),Res, ex=100)
+            print("at 0")
+            print(r.get(str(Sums)))
